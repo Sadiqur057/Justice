79ca434775746f73ad630a6db09e1c8921c2feb6
Sadiqur057 <115367918+Sadiqur057@users.noreply.github.com>
2024-01-28 07:14:17 +0600

    initialized justice project setup

---
+++ scripts/script.js
@@ -0,0 +1,17 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        clifford: '#da373d',
+        'c-primary': '#B68C5A',
+        'c-secondary': '#707070',
+        'c-third': '#13131899',
+        'c-header': '#414040',
+        'c-footer': '#ffffff99',
+        'c-border': '#1313181a',
+        'c-input':'#13131808',
+        'c-input-text':'#1313184d'
+      }
+    }
+  }
+}
